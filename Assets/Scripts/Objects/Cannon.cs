033debc8806f573a5b39fa9ec588fe64655558ec
Carlos <carlosvalladares2109@gmail.com>
2026-01-16 11:46:14 -0600

    canon particles

--- Assets/Scripts/Objects/Cannon.cs
+++ Assets/Scripts/Objects/Cannon.cs
@@ -5,6 +5,8 @@ public class Cannon : MonoBehaviour
     public GameObject projectilePrefab;
     public Transform firePoint;
     public float shootForce = 15f;
+
+    [SerializeField] ParticleSystem cannonParticles;
     
     private bool canShoot = true;
     private float timeVal = 0;
@@ -21,14 +23,20 @@ public class Cannon : MonoBehaviour
         transform.rotation = Quaternion.Euler(0, 0, angle);
 
         // 2. Disparo (Click izquierdo)
-        if (Input.GetMouseButtonDown(0) && canShoot)
+        if (canShoot)
+        {
+             if (Input.GetMouseButtonDown(0) )
         {
             Shoot();
         }
+        }
+       
     }
 
     void Shoot()
     {
+        PlayParticles();
+
         canShoot = false;
         GameObject p = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
         // Destruir bala después de 3 seg si no choca con nada para limpiar memoria
@@ -39,5 +47,11 @@ public class Cannon : MonoBehaviour
     public void ResetCannon()
     {
         canShoot = true;
+    }
+    public void PlayParticles()
+    {
+        // Reinicia completamente el sistema (padre + hijos)
+        cannonParticles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        cannonParticles.Play(true);
     }
 }
